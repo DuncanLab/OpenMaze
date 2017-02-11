e96aff52d9d6539aea514eb514dbf33f1be6b011
StivoEgg <iliaoyin@Gmail.com>
2017-02-11 15:58:59 -0500

    first commit

---
+++ Assets/Let's Try Shooter/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public Text countText;
+	public Text foundText;
+	public int totalPickUpCounts;
+public float foundTextShowTime;
+
+	private Rigidbody rb;
+	private int count;
+	private float foundTextOutTime;
+	private RawImage foundTextObj;
+	public RawImage nextMaze;
+
+	private float finishedTime;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;	
+		SetCountText ();
+		foundTextObj = foundText.GetComponentsInParent<RawImage>()[0];
+		foundTextObj.gameObject.SetActive(false);
+		nextMaze.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (foundText.IsActive ()) {
+			if (Time.time > foundTextOutTime)
+				foundTextObj.gameObject.SetActive (false);
+		}
+
+		if (nextMaze.IsActive ()) {
+			if (Time.time >= finishedTime + 5.0f) {
+				Application.LoadLevel ("Maze1");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			if (count == totalPickUpCounts) {
+				
+				nextMaze.gameObject.SetActive (true);
+				float finishedTime = Time.time;
+
+			} else {
+				SetCountText ();
+				ParticleEffectController pec = other.transform.parent.gameObject.GetComponent<ParticleEffectController> ();
+				pec.PickedUp ();
+				ShowFoundText ();
+			}
+		}
+
+
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString () + " out of " + totalPickUpCounts.ToString () + " picked up";
+	}
+
+	void ShowFoundText (){
+		foundTextObj.gameObject.SetActive(true);
+		foundTextOutTime = Time.time + foundTextShowTime;
+	}
+}
---
+++ Assets/Let's Try Shooter/Scripts/RayViewer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayViewer : MonoBehaviour {
+
+	public float weaponRange = 50f;
+
+	private Camera fpsCam;
+
+	void Start () {
+		fpsCam = GetComponentInParent<Camera> ();
+	}
+	
+	void Update () {
+		Vector3 lineOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (.5f, .5f, 0));
+		Debug.DrawRay (lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+	}
+}
