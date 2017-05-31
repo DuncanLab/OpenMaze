632471ddaa9718d8eda04d018483f08390fa7679
darknal43 <hongywang@hotmail.com>
2017-05-31 11:16:28 -0400

    New version

---
+++ NewProjectCodeBase/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    private int collectedAmount;
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private void setText()
+    {
+        countText.text = "Count: " + collectedAmount.ToString();
+        winText.text = "";
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collectedAmount = 0;
+        setText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHoriziontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriziontal, 0, moveVertical);
+        
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            collectedAmount++;
+            setText();
+        }
+        if (collectedAmount == 12)
+        {
+            winText.text = "You Win !";
+        }
+    }
+
+}
