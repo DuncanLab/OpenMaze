24c9bb83113b918d71acaa653d6f26f163730f01
darknal43 <hongywang@hotmail.com>
2017-07-19 12:15:29 -0400

    Added rider'

---
+++ Assets/Scripts/ExperimentSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+//This class will be our Experiment Singleton system for the goal of cleaner code!!!
+public class ExperimentSingleton
+{
+
+	//Classic singleton design pattern XD
+	private static ExperimentSingleton exp;
+
+	public static ExperimentSingleton getInstance(){
+		if (exp == null) exp = new ExperimentSingleton();
+
+		return exp;
+	}
+
+
+	private ExperimentSingleton ()
+	{
+	}
+}
+
+
