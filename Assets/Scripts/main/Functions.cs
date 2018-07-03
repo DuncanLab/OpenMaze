14e26c785d5fd89aa9a6a50a2a8b277fee46b608
Cody Howarth <codyhowarth@gmail.com>
2018-07-03 13:48:05 -0400

    Update - option to use one value on threshold
    - The "EndGoal" for the threshold function can now be used
    with only 1 value (an absolute number of successes)

--- Assets/Scripts/main/Functions.cs
+++ Assets/Scripts/main/Functions.cs
@@ -6,7 +6,7 @@ using E = main.Loader;
 using DS = data.DataSingleton;
 
 namespace main
-{    
+{
     //Collection of classes that are called with reflection.
     //These are called at the end of the trail and are associated with the given pickup file.
     //Return true to repeat
@@ -24,22 +24,45 @@ namespace main
 
             var numSuccessfulInPrevious = bd.EndGoal.Split(' ');
             var numSuccessfulRequired = float.Parse(numSuccessfulInPrevious[0]);
-            int previousTrialsToCheck = int.Parse(numSuccessfulInPrevious[1]);
 
-            // Automatically continue if we haven't reached the required run length
-            if (tp.successes.Count < previousTrialsToCheck) return true;
+            var successCount = 0;
 
-            var successCountInRunLength = 0;
-            for (int i = tp.successes.Count - previousTrialsToCheck; i < tp.successes.Count; i++)
+            if (numSuccessfulInPrevious.Length == 0)
             {
-                if (tp.successes[i] == 1) successCountInRunLength++;
+                Debug.LogError("Missing arguments for threshold function in config");
             }
 
-            Debug.Log(string.Format(
-                "Number of Successes required: {0}, Actual: {1}, Previous trials to check: {2}", 
-                numSuccessfulRequired, successCountInRunLength, previousTrialsToCheck));
-            
-            return successCountInRunLength < numSuccessfulRequired;
+            // User has both values set in the config
+            if (numSuccessfulInPrevious.Length == 2)
+            {
+                int previousTrialsToCheck = int.Parse(numSuccessfulInPrevious[1]);
+
+                // Continue if we haven't reached the required run length
+                if (tp.successes.Count < previousTrialsToCheck) return true;
+
+                for (int i = tp.successes.Count - previousTrialsToCheck; i < tp.successes.Count; i++)
+                {
+                    if (tp.successes[i] == 1) successCount++;
+                }
+
+                Debug.Log(string.Format(
+                    "Number of Successes required: {0}, Actual: {1}, Previous trials to check: {2}",
+                    numSuccessfulRequired, successCount, previousTrialsToCheck));
+
+            }
+            else
+            {
+                for (int i = 0; i < tp.successes.Count; i++)
+                {
+                    if (tp.successes[i] == 1) successCount++;
+                }
+
+                Debug.Log(string.Format(
+                    "Number of Successes required: {0}, Actual: {1}, Previous trials to check: ALL",
+                    numSuccessfulRequired, successCount));
+            }
+
+            return successCount < numSuccessfulRequired;
         }
     }
 }
