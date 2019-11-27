3f0ba9e0f621d7aefd88789835606acd6c761d64
Cody Howarth <codyhowarth@gmail.com>
2019-11-26 21:53:10 -0500

    Update - add timestamp instead of time counters
    - Using the DateTime class will get us a precision of ~15ms (or better).
    If we want better precision, we'll have to move to using StopWatch.

--- Assets/Scripts/main/Loader.cs
+++ Assets/Scripts/main/Loader.cs
@@ -55,7 +55,7 @@ namespace main
             using (var writer = new StreamWriter("Assets/OutputFiles~/" + DS.GetData().OutputFile, false))
             {
                 writer.Write(
-                    "Trial Increment, Trial Number, Time Since Experiement Start (Milliseconds), Time Since Trial Start (Milliseconds), X, Y, Angle, Environment Type, Sides, TargetFound, PickupType, " +
+                    "Timestamp, Trial Increment, Trial Number, X, Y, Angle, Environment Type, Sides, TargetFound, PickupType, " +
                     "TargetX, TargetY, LastX, LastY, BlockID, TrialID, Subject, Delay, 2D, Instructional, Visible, UpArrow, DownArrow," +
                     " LeftArrow, RightArrow, Space, Session, Note"
                 + "\n");
@@ -85,8 +85,8 @@ namespace main
 
                     var str = string.Format(
                         "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, " +
-                        "{12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}",
-                        data.DataSingleton.GetData().TrialInitialValue, s.TrialNumber, timeSinceExperimentStart, timeSinceTrialStart, t.position.x, t.position.z, t.eulerAngles.y, s.EnvironmentType, s.CurrentMazeName,
+                        "{12}, {13}, {14}, {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}",
+                        DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt"), data.DataSingleton.GetData().TrialInitialValue, s.TrialNumber, t.position.x, t.position.z, t.eulerAngles.y, s.EnvironmentType, s.CurrentMazeName,
                         targetFound, s.PickupType, s.TargetX, s.TargetY, s.LastX, s.LastY, s.BlockID, trialIdStr,
                         s.Subject, s.Delay, s.TwoDim, s.Instructional, s.Visible, Input.GetKey(KeyCode.UpArrow) ? 1 : 0,
                         Input.GetKey(KeyCode.DownArrow) ? 1 : 0, Input.GetKey(KeyCode.LeftArrow) ? 1 : 0,
