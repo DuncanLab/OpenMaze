dd78860f2178b3873b838d4fbe5ea3339f808aa8
Hongyu <hongyu.wang@mail.utoronto.ca>
2020-05-16 00:21:37 -0400

    Add scene wrappers for unittestability

---
+++ Assets/Scripts/unity/wrapper/ISceneWrapper.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+namespace unity.wrapper
+{
+    // Wrap all the APIs that we want to use in the wrapper service for testability.
+    public interface ISceneWrapper
+    {
+        AsyncOperation LoadAsyncScene(string name);
+        AsyncOperation LoadAsyncScene(int environmentNumber);
+    }
+}
---
+++ Assets/Scripts/unity/wrapper/SceneWrapper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace unity.wrapper
+{
+    public class SceneWrapper : ISceneWrapper
+    {
+        private static ISceneWrapper _sceneWrapper;
+        
+        // Singleton service
+        public static ISceneWrapper Create()
+        {
+            return _sceneWrapper ?? (_sceneWrapper = new SceneWrapper());
+        }
+        
+        
+        public AsyncOperation LoadAsyncScene(string name)
+        {
+            return SceneManager.LoadSceneAsync(name);
+        }
+        
+        public AsyncOperation LoadAsyncScene(int environmentNumber)
+        {
+            return SceneManager.LoadSceneAsync(environmentNumber);
+        }
+    }
+}
